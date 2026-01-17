bdae2340235845ad880edc35621f6595f0441711
matheusgomes2052 <matheusgomes2052@gmail.com>
2026-01-17 16:26:01 -0300

    Adicionado Cabeçote
    Alterado espinha dorsal do código de Function para Class;
    Adicionado cabeçote no motor;
    Adicionado controle das válvulas no painel;
    Ajustado erros.

--- main.js
+++ main.js
@@ -3,13 +3,14 @@ document.addEventListener("DOMContentLoaded", function (){
 		input.addEventListener("input", function (){
 			let velocidade_selecionada = document.getElementById("velocidade").value;
 			let biela = document.getElementById("biela").value;
+			let valvula = document.getElementById("valvula").value;
 			let pistoes = [];
 			document.querySelectorAll("[data-type='pistao']").forEach(function (input_pistao){
 				if (input_pistao.value != ''){
 					pistoes.push(input_pistao.value);
 				}
 			})
-			document.getElementById("iframe").src = "motor.html?biela="+biela+"&velocidade="+velocidade_selecionada+"&pistoes="+pistoes.join("+");
+			document.getElementById("iframe").src = "motor.html?biela="+biela+"&valvula="+valvula+"&velocidade="+velocidade_selecionada+"&pistoes="+pistoes.join("+");
 		})
 	})
 })
--- motor.js
+++ motor.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
 	class motor{
 		constructor(angulos){
 			this.angulos_pistoes = angulos;
-			this.velocidade = 0.5;
-			this.alturaBiela = 150;
+			this.velocidade = 2;
+			this.alturaBiela = 80;
 			this.larguraBiela = 20;
 			this.quantia_aneis = 3;
 			this.borda_embaixo = 100;
@@ -16,7 +16,9 @@ document.addEventListener("DOMContentLoaded", () => {
 			this.larguraCabecaPistao = 80;
 			this.alturaCabecaPistao = 40;
 			this.paraMotor = 0;
+			this.alturaCabecote = 80;
 			this.margemManivela = 4;
+			this.variacaoValvula = 50;
 			this.containerMotor = document.querySelector(".container-motor");
 			this.insereEspacador(20, this.grossuraVirabrequim);
 			this.contPistao = 1;			
@@ -27,6 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
 				this.anguloPistaoForeach = angulo;
 				this.insereManivela(this.larguraManivela);
 				this.insereCamisa();
+				this.insereCabecote();
 				this.inserePistao();				
 				this.insereManivela(this.larguraManivela);
 				this.insereEspacador(20, this.grossuraVirabrequim);
@@ -76,6 +79,16 @@ document.addEventListener("DOMContentLoaded", () => {
 			pistao.className = "pistao";
 			pistao.dataset.pistao = this.contPistao;
 			pistao.dataset.angulo = this.anguloPistaoForeach;
+			let anguloTempo = this.anguloPistaoForeach % 720;
+			if (anguloTempo < 180){
+				pistao.dataset.tempo = "i";
+			}else if (anguloTempo >= 180 && anguloTempo < 360){
+				pistao.dataset.tempo = "c";
+			}else if (anguloTempo >= 360 && anguloTempo < 520){
+				pistao.dataset.tempo = "p";
+			}else if (anguloTempo >= 520){
+				pistao.dataset.tempo = "e";
+			}
 			pistao.style.left = this.left_quantia - ((this.larguraCabecaPistao - this.larguraBronzina) / 2) + "px";
 			pistao.style.bottom = (this.borda_embaixo - ((this.alturaBronzina - this.grossuraVirabrequim) / 2)) + "px";
 			this.left_quantia = this.left_quantia + this.larguraBronzina;
@@ -85,14 +98,50 @@ document.addEventListener("DOMContentLoaded", () => {
 			let camisa = document.createElement("div");
 			camisa.className = "camisa";
 			camisa.style.width = (this.larguraCabecaPistao + 4) + "px";
-			this.alturaCamisa = (2 * this.alturaManivela) + 10;
+			this.alturaCamisa = (2 * this.alturaManivela) + 10 + this.alturaCabecaPistao - (2 * this.grossuraVirabrequim);
 			camisa.style.height = this.alturaCamisa + "px";
-			camisa.style.left = (this.left_quantia - ((this.larguraCabecaPistao - this.larguraBronzina) / 2) - 2) + "px";
-			camisa.style.bottom = (this.borda_embaixo + this.variacao + this.grossuraVirabrequim + this.margemManivela) + "px";
+			this.leftCamisa = (this.left_quantia - ((this.larguraCabecaPistao - this.larguraBronzina) / 2) - 2)
+			camisa.style.left = this.leftCamisa + "px";
+			camisa.style.bottom = (this.borda_embaixo + this.variacao + this.grossuraVirabrequim + this.margemManivela + 1) + "px";
 			this.containerMotor.appendChild(camisa);
 		}
+		insereCabecote(){
+			let cabecote = document.createElement("div");
+			cabecote.className = "cabecote";
+			cabecote.style.width = this.larguraCabecaPistao + 4 + "px";
+			cabecote.style.height = this.alturaCabecote + "px";
+			cabecote.style.left = this.leftCamisa + "px";
+			cabecote.dataset.cabecote = this.contPistao;
+			cabecote.style.bottom = (this.borda_embaixo + this.variacao + this.grossuraVirabrequim + this.margemManivela + this.alturaCamisa + 1) + "px";
+			for(let i = 1; i <= 2; i++){
+				let valvula = document.createElement("div");
+				valvula.className = "valvula";
+				valvula.dataset.valvula = i;
+				let pontaValvula = document.createElement("div");
+				pontaValvula.className = "ponta-valvula";
+				let corpoValvula = document.createElement("div");
+				corpoValvula.className = "corpo-valvula";
+				valvula.appendChild(corpoValvula);
+				valvula.appendChild(pontaValvula);
+				cabecote.appendChild(valvula);
+			}
+			this.containerMotor.appendChild(cabecote);
+		}
 		atualizaPecas(){
-
+			this.alturaManivela = ((this.alturaBiela / 2) + this.alturaBronzina - this.margemManivela);
+			this.alturaCamisa = (2 * this.alturaManivela) + 10 + this.alturaCabecaPistao - (2 * this.grossuraVirabrequim);
+			this.variacao = (((this.alturaBronzina - this.grossuraVirabrequim) / 2) + this.alturaManivela - this.alturaBronzina);
+			document.querySelectorAll(".camisa").forEach((camisa) => {
+				camisa.style.height = this.alturaCamisa + "px";
+				console.log([this.borda_embaixo, this.alturaBronzina, this.alturaBiela, this.variacao])
+				camisa.style.bottom = (this.borda_embaixo + this.alturaBronzina + this.alturaBiela - this.variacao) + "px";
+			})
+			document.querySelectorAll(".biela").forEach((biela) => {
+				biela.style.height = this.alturaBiela + "px";
+			})
+			document.querySelectorAll(".cabecote").forEach((cabecote) => {
+				cabecote.style.bottom = (this.borda_embaixo + this.variacao + this.grossuraVirabrequim + this.margemManivela + this.alturaCamisa + 1) + "px";
+			})
 		}
 		trave(){
 			this.paraMotor = 1;
@@ -104,7 +153,31 @@ document.addEventListener("DOMContentLoaded", () => {
 				let numPistao = pistao.dataset.pistao;
 				let anguloPistao = (Number(pistao.dataset.angulo) + this.anguloAtual) % 360;
 				this.radiano = (anguloPistao / 180) * Math.PI;
-				this.cos = Math.cos(this.radiano);				
+				this.cos = Math.cos(this.radiano);
+				let valvulas = document.querySelector("[data-cabecote='"+numPistao+"']").querySelectorAll('.valvula');
+				let translateValvula = ((Math.abs(Math.sin(this.radiano))) * (this.variacaoValvula / 2));
+
+				if (anguloPistao < 180 && pistao.dataset.tempo == "e"){
+					pistao.dataset.tempo = "i";
+				}else if (anguloPistao < 180 && pistao.dataset.tempo == "c"){
+					pistao.dataset.tempo = "p";
+				}else if (anguloPistao >= 180 && pistao.dataset.tempo == "i"){
+					pistao.dataset.tempo = "c";
+				}else if (anguloPistao >= 180 && pistao.dataset.tempo == "p"){
+					pistao.dataset.tempo = "e";
+				}
+
+				if (pistao.dataset.tempo == "i"){
+					valvulas[0].style.transform = "translateY("+translateValvula+"px)";
+				}else if (pistao.dataset.tempo == "e"){
+					valvulas[1].style.transform = "translateY("+translateValvula+"px)";
+				}else{
+					valvulas.forEach((valv) => {
+						valv.style.transform = "translateY(0px)";
+					})
+				}
+
+
 				this.alturaManivela = ((this.alturaBiela / 2) + this.alturaBronzina - this.margemManivela);
 				let alturaAtualManivela = (((this.alturaManivela - this.grossuraVirabrequim) * Math.abs(this.cos)) + this.grossuraVirabrequim);
 				document.querySelectorAll("[data-num-pistao='"+numPistao+"']").forEach((manivela) => {
@@ -119,10 +192,8 @@ document.addEventListener("DOMContentLoaded", () => {
 				console.log("alt: "+this.alturaManivela+" var: "+this.variacao);
 				pistao.style.transform = "translateY("+(this.variacao * this.cos)+"px)";
 				if (anguloPistao == 0 || anguloPistao == 180){
-					// console.log((this.variacao * this.cos));
 				}
 			})
-			this.atualizaPecas();
 			if (this.paraMotor == 0){
 				setTimeout(() => this.gire(), 0);
 			}else{
@@ -130,6 +201,11 @@ document.addEventListener("DOMContentLoaded", () => {
 			}
 		}
 	}
-	let pistoes = [270, 180, 90, 0];
+	const params = new URLSearchParams(window.location.search);
+	let pistoes = params.get("pistoes").split(" ");
 	let motor_obj = new motor(pistoes);
+	motor_obj.alturaBiela = Number(params.get("biela"));
+	motor_obj.variacaoValvula = Number(params.get('valvula'));
+	motor_obj.velocidade = Number(params.get("velocidade"));	
+	motor_obj.atualizaPecas();
 })
